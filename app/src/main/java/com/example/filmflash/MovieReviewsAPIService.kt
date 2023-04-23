92d289e381a0f75a9095daaf0c37420a7e0f1845
Benjamin Effress <126579023+Ben-Effress@users.noreply.github.com>
2023-04-23 16:10:29 -0400

    API Backend functionality making requests to the now playing and reviews endpoints to get data for currently playing movies.

---
+++ app/src/main/java/com/example/filmflash/MovieReviewsAPIService.kt
@@ -0,0 +1,34 @@
+package com.example.filmflash
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Path
+
+private const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+private const val API_KEY = "4681e090d7dce827b558f805c05e0c8a"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MovieReviewsAPIService {
+    @GET("{movie_id}/reviews?api_key=$API_KEY")
+    fun getCoursesFromAPI(@Path("movieID") movieID: Int):
+            Call<MutableList<Review>>
+}
+
+object MovieReviewsAPI {
+    val retrofitService : MovieReviewsAPIService by lazy {
+        retrofit.create(MovieReviewsAPIService::class.java)
+    }
+}
