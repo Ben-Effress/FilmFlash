04ca26d62721028dffb68f4f0bfb30a4c58bef6c
Benjamin Effress <126579023+Ben-Effress@users.noreply.github.com>
2023-04-26 20:28:00 -0400

    api service with info fragment

--- app/src/main/java/com/example/filmflash/MovieInfoFragment.kt
+++ app/src/main/java/com/example/filmflash/MovieInfoFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.filmflash.databinding.FragmentMovieInfoBinding
 
 class MovieInfoFragment : Fragment() {
 
     private var _binding: FragmentMovieInfoBinding? = null
     private val binding get() = _binding!!
+    private var movieID = 0
+
+    private lateinit var viewModel: MovieInfoViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +23,17 @@ class MovieInfoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentMovieInfoBinding.inflate(inflater, container, false)
+        arguments?.let {
+            movieID = it.getInt("movieID")
+        }
 
+        viewModel = ViewModelProvider(this)[MovieInfoViewModel::class.java]
+        viewModel.getMovieInfo(movieID)
+        val textView = binding.textView
+        textView.text = movieID.toString()
+        viewModel.movieInfo.observe(this, Observer {
+            textView.text = it.title
+        })
         return binding.root
     }
 
