df0e5874c48be5c0404f807acd7d74f347d113ff
Benjamin Effress <126579023+Ben-Effress@users.noreply.github.com>
2023-04-26 21:48:18 -0400

    started inflating

--- app/src/main/java/com/example/filmflash/MovieInfoFragment.kt
+++ app/src/main/java/com/example/filmflash/MovieInfoFragment.kt
@@ -1,17 +1,20 @@
 package com.example.filmflash
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.filmflash.databinding.FragmentMovieInfoBinding
+import com.bumptech.glide.Glide
+import com.example.filmflash.databinding.FragmentMovieDetailsBinding
 
 class MovieInfoFragment : Fragment() {
 
-    private var _binding: FragmentMovieInfoBinding? = null
+    private var _binding: FragmentMovieDetailsBinding? = null
     private val binding get() = _binding!!
     private var movieID = 0
 
@@ -23,7 +26,7 @@ class MovieInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentMovieInfoBinding.inflate(inflater, container, false)
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
         arguments?.let {
             movieID = it.getInt("movieID")
         }
@@ -34,13 +37,35 @@ class MovieInfoFragment : Fragment() {
         reviewsViewModel = ViewModelProvider(this)[MovieReviewsViewModel::class.java]
         reviewsViewModel.getReviewsList(requireContext(), movieID)
 
-        val textView = binding.textView
-
         viewModel.movieInfo.observe(this, Observer {
-            textView.text = it.title
+            binding.apply {
+                movieTitle.text = it.title
+                movieRatingBar.rating = (it.vote_average/2).toFloat()
+                val backdropPath = "https://image.tmdb.org/t/p/original/" + it.backdrop_path.toString()
+                Glide.with(requireContext()).load(backdropPath).into(movieBackdrop)
+                val hours = it.runtime/60
+                val mins = it.runtime%60
+                movieLength.text = hours.toString() + "h " + mins.toString() +"m"
+                if(it.adult) {
+                    movieAdult.text = "Yes"
+                } else {
+                    movieAdult.text = "No"
+                }
+                movieRating.text = String.format("%.1f", it.vote_average) + "/10"
+                movieVotes.text = it.vote_count.toString() + " votes"
+                if (it.tagline == "") {
+                    movieTagline.visibility = View.GONE
+                }
+                movieReleaseDate.text = it.release_date
+
+
+            }
+
+
+
         })
 
-        val textView2 = binding.textView2
+        val textView2 = binding.reviewsTitle
         reviewsViewModel.reviewList.observe(this, Observer {
             textView2.text = it[0].author
         })
--- app/src/main/java/com/example/filmflash/ReviewFragment.kt
+++ app/src/main/java/com/example/filmflash/ReviewFragment.kt
@@ -6,11 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.example.filmflash.databinding.FragmentReviewBinding
+import com.example.filmflash.databinding.FragmentReviewDetailsBinding
 
 class ReviewFragment : Fragment() {
 
-    private var _binding: FragmentReviewBinding? = null
+    private var _binding: FragmentReviewDetailsBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: MovieReviewsViewModel
 
@@ -19,11 +19,9 @@ class ReviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentReviewBinding.inflate(inflater, container, false)
+        _binding = FragmentReviewDetailsBinding.inflate(inflater, container, false)
 
         viewModel = ViewModelProvider(this)[MovieReviewsViewModel::class.java]
-
-        val textView = binding.textView
 
 
 
