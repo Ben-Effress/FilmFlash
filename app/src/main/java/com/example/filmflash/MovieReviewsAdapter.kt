269a5c3bddc807b5a501450298c70c2c7326912f
Benjamin Effress <126579023+Ben-Effress@users.noreply.github.com>
2023-04-26 22:45:30 -0400

    movie info inflated

---
+++ app/src/main/java/com/example/filmflash/MovieReviewsAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.filmflash
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.filmflash.databinding.ReviewItemBinding
+
+class MovieReviewsAdapter(private val reviewList: MutableList<ReviewWithAll>):
+    RecyclerView.Adapter<MovieReviewsAdapter.ReviewItemHolder>() {
+
+    private var _binding: ReviewItemBinding? = null
+    private val binding get() = _binding!!
+
+    inner class ReviewItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+                // Triggers click upwards to the adapter on click
+                val position = absoluteAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(itemView, position)
+                }
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: MovieReviewsAdapter.ReviewItemHolder, position: Int) {
+        val review = reviewList[position]
+
+        binding.apply {
+//            val avatarPath = "https://image.tmdb.org/t/p/original/" + review.authorDetails.avatarPath
+//            Glide.with(holder.itemView.context).load(avatarPath).into(reviewItemProfilePicImage)
+            reviewItemUsername.text = review.author
+            reviewItemContent.text = review.contentPreview
+            Log.i("Rating", review.authorDetails.rating.toString())
+            reviewItemRatingBar.rating = review.authorDetails.rating.toFloat() ?: 0f
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieReviewsAdapter.ReviewItemHolder {
+        _binding = ReviewItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent,
+            false)
+        return ReviewItemHolder(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return reviewList.size
+    }
+
+    // Define the listener interface
+    interface OnItemClickListener {
+        fun onItemClick(itemView: View?, position: Int)
+    }
+
+    // Define listener member variable
+    private lateinit var listener: OnItemClickListener
+
+    // Define the method that allows the parent activity or fragment to define the listener
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+}
